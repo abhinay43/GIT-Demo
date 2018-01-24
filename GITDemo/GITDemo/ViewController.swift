4fe3c0f98cac766085be42108f58f3cb7240f116
Kumar_Bunty <abhinay.mca09@gmail.com>
2018-01-24 14:09:37 +0530

    View Controller: GIT 2 Changes

--- GITDemo/GITDemo/ViewController.swift
+++ GITDemo/GITDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //GIT 2 Changes
         // Do any additional setup after loading the view, typically from a nib.
     }
 
